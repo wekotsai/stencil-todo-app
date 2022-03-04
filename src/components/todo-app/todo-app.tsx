b8a18f20ab3de7a799a1a5bc6f29cd98a5949a5d
Wéko <42733086+wekotsai@users.noreply.github.com>
2022-03-04 11:23:39 +0100

    :art: add checkbox

--- src/components/todo-app/todo-app.tsx
+++ src/components/todo-app/todo-app.tsx
@@ -9,6 +9,7 @@ export class TodoApp {
   @Prop() item: string;
   @State() value: string;
   @State() list = [];
+  @State() checkboxValue: string;
 
   inputHandler(event) {
     this.value = event.target.value;
@@ -25,12 +26,21 @@ export class TodoApp {
   }
 
   addItem() {
-    this.list = this.list.concat([this.value]);
-    this.clearInput();
+    if (this.value.length !== 0) {
+      this.list = this.list.concat([this.value]);
+      this.clearInput();
+    }
   }
 
   removeItem(value) {
     this.list = this.list.filter(item => item !== value);
+  }
+
+  checkboxHandler(event) {
+    this.checkboxValue = event.target.value;
+    if(this.checkboxValue === 'on') {
+      console.log('checked');
+    }
   }
 
   clearInput() {
@@ -40,11 +50,13 @@ export class TodoApp {
  render() {
    return (
     <div class="todo-app">
-      <input value={this.value} type="text" onInput={(event) => this.inputHandler(event)} onKeyUp={(event) => this.keyUpHandler(event)}/>
+      <input class="field" value={this.value} type="text" onInput={(event) => this.inputHandler(event)} onKeyUp={(event) => this.keyUpHandler(event)}/>
       <div>
         <ul class="list">
         {(this.list).map(value => (
-          <li class="item">{value} <button onClick={()=>this.removeItem(value)}>x</button></li>
+          <li class="item"><input class="checkbox" type="checkbox" value={this.checkboxValue} onChange={this.checkboxHandler.bind(this)}/>
+            {value} <button onClick={()=>this.removeItem(value)}>x</button>
+          </li>
         ))}
         </ul>
       </div>
--- www/build/todo-app.entry.js
+++ www/build/todo-app.entry.js
@@ -1,6 +1,6 @@
 import { r as registerInstance, h } from './index-c37bab2d.js';
 
-const todoAppCss = ".todo-app{text-align:center}.list{list-style:none;width:30%;margin:0 auto;text-align:left;padding:0.5rem}.item{border-bottom:1px solid lightgray;padding:0.5rem}input{padding:0.5rem 1rem;width:30%;border:none;border-radius:10px;box-shadow:0 0.125rem 0.625rem 0 rgb(0 0 0 / 20%)}button{border:2px black solid;background-color:white;border-radius:50px;float:right}";
+const todoAppCss = ".todo-app{text-align:center;max-width:450px;margin:0 auto}.list{list-style:none;width:100%;margin:0 auto;text-align:left;padding:0.5rem}.item{border-bottom:1px solid lightgray;padding:0.5rem}.field{padding:0.5rem 1rem;width:100%;border:none;border-radius:10px;box-shadow:0 0.125rem 0.625rem 0 rgb(0 0 0 / 20%)}.checkbox{margin-right:1rem}button{border:2px black solid;background-color:white;border-radius:50px;float:right}";
 
 let TodoApp = class {
   constructor(hostRef) {
@@ -20,17 +20,25 @@ let TodoApp = class {
     }
   }
   addItem() {
-    this.list = this.list.concat([this.value]);
-    this.clearInput();
+    if (this.value.length !== 0) {
+      this.list = this.list.concat([this.value]);
+      this.clearInput();
+    }
   }
   removeItem(value) {
     this.list = this.list.filter(item => item !== value);
+  }
+  checkboxHandler(event) {
+    this.checkboxValue = event.target.value;
+    if (this.checkboxValue === 'on') {
+      console.log('checked');
+    }
   }
   clearInput() {
     this.value = '';
   }
   render() {
-    return (h("div", { class: "todo-app" }, h("input", { value: this.value, type: "text", onInput: (event) => this.inputHandler(event), onKeyUp: (event) => this.keyUpHandler(event) }), h("div", null, h("ul", { class: "list" }, (this.list).map(value => (h("li", { class: "item" }, value, " ", h("button", { onClick: () => this.removeItem(value) }, "x"))))))));
+    return (h("div", { class: "todo-app" }, h("input", { class: "field", value: this.value, type: "text", onInput: (event) => this.inputHandler(event), onKeyUp: (event) => this.keyUpHandler(event) }), h("div", null, h("ul", { class: "list" }, (this.list).map(value => (h("li", { class: "item" }, h("input", { class: "checkbox", type: "checkbox", value: this.checkboxValue, onChange: this.checkboxHandler.bind(this) }), value, " ", h("button", { onClick: () => this.removeItem(value) }, "x"))))))));
   }
 };
 TodoApp.style = todoAppCss;
