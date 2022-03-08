ebec323743f7dd5b416866fb4537c6e0398166bc
Wéko <42733086+wekotsai@users.noreply.github.com>
2022-03-08 16:37:39 +0100

    :construction: reset netlify

--- www/build/index.esm.js
+++
@@ -1,8 +0,0 @@
-import { A as ActiveRouter } from './active-router-75cf6213.js';
-import './match-path-760e1797.js';
-import './index-c37bab2d.js';
-import './location-utils-fea12957.js';
-
-function injectHistory(Component) {
-    ActiveRouter.injectProps(Component, ['history', 'location']);
-}
