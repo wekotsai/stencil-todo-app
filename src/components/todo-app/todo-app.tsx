a59d3becf0d925fcd34f7e6f99f47635be581d36
Wéko <42733086+wekotsai@users.noreply.github.com>
2022-03-18 16:31:05 +0100

    fix duplicated items and filter button highlight

--- src/components/todo-app/todo-app.tsx
+++ src/components/todo-app/todo-app.tsx
@@ -8,10 +8,10 @@ import {Component, Prop, State, h} from '@stencil/core';
 export class TodoApp {
   @Prop() item: string;
   @State() value: string;
-  // always deifne types
   @State() list: string[] = [];
-  @State() checked: string[] = [];
+  @State() checked: number[] = [];
   @State() show: 'all' | 'active' | 'completed' = 'all';
+  @State() selected: boolean = false;
 
   get itemsLeft() {
     return this.list.length - this.checked.length;
@@ -21,8 +21,8 @@ export class TodoApp {
     const {list, checked} = this;
     switch (this.show) {
       case 'all': return list;
-      case 'active': return list.filter(item => !checked.includes(item));
-      case 'completed': return checked;
+      case 'active': return list.filter((_item, index) => !checked.includes(index));
+      case 'completed': return list.filter((_item, index) => checked.includes(index));
     }
   }
 
@@ -50,22 +50,22 @@ export class TodoApp {
     this.clearInput();
   }
 
-  removeItem(value) {
-    this.list = this.list.filter(item => item !== value);
-    this.checked = this.checked.filter(item => item !== value);
+  removeItem(index) {
+    const checkedMap = this.checked.map((oldIndex) => oldIndex === index ? null : this.list[oldIndex]);
+    this.list = this.list.slice(0, index).concat(this.list.slice(index + 1));
+    this.checked = checkedMap.filter(item => item !== null).map((item) => this.list.indexOf(item));
   }
 
-  // pass value and type
-  checkboxHandler(value:string) {
-    if (this.isChecked(value)) {
-      this.checked = this.checked.filter(item => item !== value);
+  checkboxHandler(index:number) {
+    if (this.isChecked(index)) {
+      this.checked = this.checked.filter((item) => item !== index);
     } else {
-      this.checked = this.checked.concat([value]);
+      this.checked = this.checked.concat([index]);
     }
   }
 
-  isChecked(value:string) {
-    return this.checked.includes(value);
+  isChecked(index:number) {
+    return this.checked.includes(index);
   }
 
   clearInput() {
@@ -76,12 +76,12 @@ export class TodoApp {
     if (this.isAllChecked) {
       this.checked = [];
     } else {
-      this.checked = this.list;
+      this.checked = this.list.map((_item, index) => index) ;
     }
   }
 
   clearCompleted() {
-    this.list = this.list.filter(value => !this.checked.includes(value));
+    this.list = this.list.filter((_value, index) => !this.checked.includes(index));
     this.checked = [];
   }
 
@@ -94,20 +94,20 @@ export class TodoApp {
       <label htmlFor="toggleAll" class={{toggleAll: true, 'toggleAll--active': this.isAllChecked}} onClick={()=>this.toggleAll()}></label>
       <div>
         <ul class="list">
-        {(this.displayedList).map(value => (
-          <li class={this.isChecked(value) ? 'item item-checked' : 'item'}>
-            <input class="checkbox" type="checkbox" checked={this.isChecked(value)} value={value} onChange={() => this.checkboxHandler(value)}/>
-              <span class={{strike:this.isChecked(value)}}>{value}</span>
-            <button class="remove" onClick={()=>this.removeItem(value)}>x</button>
+        {(this.displayedList).map((value, index) => (
+          <li class={this.isChecked(index) ? 'item item--checked' : 'item'}>
+            <input class="checkbox" type="checkbox" checked={this.isChecked(index)} value={value} onChange={() => this.checkboxHandler(index)}/>
+              <span class={{strike:this.isChecked(index)}}>{value}</span>
+            <button class="remove" onClick={()=>this.removeItem(index)}>x</button>
           </li>
         ))}
         </ul>
         <div class="filters">
           <span class="quantity">{this.itemsLeft} item{this.itemsLeft !== 1 ? 's' : ''} left</span>
-          <button onClick={() => (this.show = 'all')}>All</button>
-          <button onClick={() => (this.show = 'active')}>Actvie</button>
-          <button onClick={() => (this.show = 'completed')}>Completed</button>
-          <button onClick={() => this.clearCompleted()} class={this.checked.length > 0 ? 'clear-show' : 'clear'}>Clear completed</button>
+          <button onClick={() => (this.show = 'all')} class={{filterButton: true, 'filterButton--active': this.show === 'all'}}>All</button>
+          <button onClick={() => (this.show = 'active')} class={{filterButton: true, 'filterButton--active': this.show === 'active'}}>Actvie</button>
+          <button onClick={() => (this.show = 'completed')} class={{filterButton: true, 'filterButton--active': this.show === 'completed'}}>Completed</button>
+          <button onClick={() => this.clearCompleted()} class={this.checked.length > 0 ? 'clear--show' : 'clear--hidden'}>Clear completed</button>
         </div>
       </div>
     </div>
